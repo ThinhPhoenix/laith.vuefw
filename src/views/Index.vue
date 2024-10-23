592e3a35e16f651a11155d1b812149fee25fddb9
ThinhPhoenix <laithinh1310@gmail.com>
2024-10-23 18:35:43 +0700

    chore: pinia setup

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,7 +3,7 @@
       <div class="w-screen h-screen blur-[0.8px] absolute"></div>
       <div class="flex justify-center items-center h-screen flex-col gap-3">
         <monalisa/>
-        <p>Learn <router-link to="/tutorial/router"><redirect>router</redirect></router-link> and <router-link to="/tutorial/tailwindcss"><redirect>tailwindcss</redirect></router-link> others the same as Vue + Vite.</p>
+        <p>Learn <router-link to="/tutorial/router"><redirect>router</redirect></router-link>, <router-link to="/tutorial/pinia"><redirect>pinia</redirect></router-link> and <router-link to="/tutorial/tailwindcss"><redirect>tailwindcss</redirect></router-link> others the same as Vue + Vite.</p>
       </div>
     </section>
   </template>
---
+++ src/views/Tutorial.Pinia.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+      <div class="bg-[#3030307e] backdrop-blur-sm rounded-lg p-6 border border-emerald-600 border-opacity-50 shadow-lg">
+        <span class="font-bold text-gray-200">
+           Run <code class="highlight">node .laith.vuefw\pinia</code> in terminal
+        </span>
+        <div>
+            <span>Back to
+            <router-link to="/">
+                <redirect class="mt-5 font-normal">root</redirect>
+            </router-link>
+        </span>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import Redirect from '@/components/Redirect.vue';
+  </script>
+  
+  <style scoped>
+  code.highlight {
+    background-color: rgba(55, 245, 112, 0.15);
+    color: #32ff6f;
+    padding: 3px;
+    border-radius: 4px;
+    border: 0.5px solid #32ff6fd2;
+  }
+  </style>  
